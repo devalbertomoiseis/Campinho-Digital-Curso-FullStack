d15289518a19d8c3b417a39c13d3d268e8246965
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-05-10 12:17:11 -0300

    aula de array

---
+++ Aulas/01-Array/array.js
@@ -0,0 +1,19 @@
+/*
+ - map;
+ - filter;
+ - find;
+ - findIndex;
+ - fill;
+ - some;
+ - every;
+ - Pesquisar !
+*/
+
+let nomes = [ 
+  "alberto", "rafaella", "renata", "sofia", "alberto"
+]
+console.log(typeof nomes);
+let numeros = [ 
+  1,2,4,8,12
+]
+console.log(typeof numeros);
