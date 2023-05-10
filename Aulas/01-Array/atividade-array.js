462c26fdcfa7973ce7786fe8260438e3325b272e
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-05-10 12:32:13 -0300

    add: atividades sobre array

---
+++ Aulas/01-Array/atividade-array.js
@@ -0,0 +1,15 @@
+const numeros = [1,4,7]
+numeros.push(10);
+console.log(numeros);
+
+const sorvetes = ["chocolate", "morango", "doce de leite"];
+sorvetes.push("sorvete de banana ");
+console.log(sorvetes);
+
+const nomes = ["Ana","Bruna","Carlos"];
+nomes.push("Daniel");
+console.log(sorvetes);
+
+const profissoes = ["Programador", "QA", "Scrum Master", "Analista"];
+const tamanhoDoArray = profissoes.length;
+console.log(tamanhoDoArray);
