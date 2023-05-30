bb1de4336a68349a27e716c96649dce8d67341c9
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-05-29 23:45:49 -0300

    att

--- Exercicios/04-Pizzaria/script.js
+++ Exercicios/04-Pizzaria/script.js
@@ -3,7 +3,7 @@ function calcularConta(){
     const pegaValorTaxa = document.getElementById("valorTaxa");
     const pegaQuantidadePessoa = document.getElementById("quantidade");
     const calcValueF = 
-    parseInt(pegaValorConta.value) + parseInt(pegaValorTaxa.value) / parseInt(pegaQuantidadePessoa.value)
+    (parseInt(pegaValorConta.value) + parseInt(pegaValorTaxa.value)) / parseInt(pegaQuantidadePessoa.value)
    
     const resultado = document.getElementById("resultado")
     resultado.innerHTML = calcValueF
